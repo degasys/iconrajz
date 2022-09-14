cdfe5dbca6aa151c66ad1db86b269295c6b76529
Debre Gábor <66725793+degasys@users.noreply.github.com>
2022-09-14 08:59:06 +0200

    main.blocks, main.ts, main.py frissítése

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+def on_button_pressed_a():
+    basic.show_string("Hello Ali!")
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_gesture_shake():
+    music.play_tone(262, music.beat(BeatFraction.WHOLE))
+    basic.show_icon(IconNames.SAD)
+    basic.pause(500)
+    basic.show_icon(IconNames.HAPPY)
+    basic.pause(500)
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+def on_button_pressed_b():
+    basic.show_string("Hello Béla!")
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
 basic.show_arrow(ArrowNames.NORTH)
 basic.pause(200)
 basic.show_arrow(ArrowNames.EAST)
--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    basic.showString("Hello Ali!")
+})
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    basic.showIcon(IconNames.Sad)
+    basic.pause(500)
+    basic.showIcon(IconNames.Happy)
+    basic.pause(500)
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    basic.showString("Hello Béla!")
+})
 basic.showArrow(ArrowNames.North)
 basic.pause(200)
 basic.showArrow(ArrowNames.East)
